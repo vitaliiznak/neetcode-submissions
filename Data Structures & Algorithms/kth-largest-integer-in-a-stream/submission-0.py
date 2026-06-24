c4d8671edee1c86bbadb8e45fe380bcb89d6d3d7
Vitalii Znak <vitaliiznak@users.noreply.github.com>
2026-06-25 00:29:25 +0200

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.py
@@ -0,0 +1,13 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.arr = nums
+        
+
+    def add(self, val: int) -> int:
+        self.arr.append(val)
+        self.arr.sort()
+        return self.arr[-self.k]
+
+        
