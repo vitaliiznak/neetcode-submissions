ca5cc640f6b0760319d874d087a991f1fbe8661c
Vitalii Znak <vitaliiznak@users.noreply.github.com>
2026-07-15 00:45:31 +0200

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        p_dict = {
+             ")": "(", "}": "{", "]": "["
+        }
+        stack = []
+        for p in s:
+            if p in p_dict:
+                if not stack or p_dict[p] != stack.pop():
+                    return False
+            else:
+                 stack.append(p)
+               
+        return not stack 
